675fff93a6154f62b0949d19e2e785f3ef6ec9b4
Goffity Corleone <goffity@gmail.com>
2020-06-30 23:44:10 +0700

    Format code.

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -1,17 +1,17 @@
 resource "digitalocean_loadbalancer" "www-lb" {
-  name = "web-lb"
+  name   = "web-lb"
   region = "sgp1"
 
   forwarding_rule {
-    entry_port = 80
+    entry_port     = 80
     entry_protocol = "http"
 
-    target_port = 80
+    target_port     = 80
     target_protocol = "http"
   }
 
   healthcheck {
-    port = 22
+    port     = 22
     protocol = "tcp"
   }
 
